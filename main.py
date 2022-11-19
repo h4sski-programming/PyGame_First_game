727a6708572bc951d229e44eff78b73cc8c0921c
h4sski programming <h4sski.programming@gmail.com>
2022-11-19 17:55:44 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+import pygame as pg
+
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RESOLUTION)
+        self.clock = pg.time.Clock()
+        self.is_running = True
+
+    def new_game(slef):
+        pass
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.is_running = False
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while self.is_running:
+            self.check_events()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
